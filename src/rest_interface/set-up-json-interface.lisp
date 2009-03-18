e00ba2800df5ee09b902fcfec1ea8342ecf6590d
lgiessmann <lgiessmann@Marcs-MBP-2.lan>
2009-03-18 14:32:50 +0000

    finished a prototype of the ajax-interface with the frameworks "prototype" and "scriptaculous" as a framework combination+ssh://lgiessmann@common-lisp.net/project/isidorus/svn

--- src/rest_interface/set-up-json-interface.lisp
+++ src/rest_interface/set-up-json-interface.lisp
@@ -14,13 +14,26 @@
    and also registers a file-hanlder to the html-user-interface"
   (declare (string json-get-prefix json-get-all-psis json-commit-url json-user-interface-url json-user-interface-file-path))
   (push ;TODO create a static-file-and-folder-handler for all static files
-   (create-regex-dispatcher json-user-interface-url #'(lambda()
-							(hunchentoot:handle-static-file json-user-interface-file-path)))
+   (create-regex-dispatcher json-user-interface-url
+			    #'(lambda()
+				(hunchentoot:handle-static-file json-user-interface-file-path "text/html")))
    hunchentoot:*dispatch-table*)
-  (push
-   (create-regex-dispatcher "/prototype-1.6.0.3.js" #'(lambda()
-							(hunchentoot:handle-static-file "json/prototype-1.6.0.3.js")))
-   hunchentoot:*dispatch-table*)
+  ;; === ajax frameworks =======================================================
+  ;; TODO: *define a new parameter which represents the folder of the javascript
+  ;;        files and load all files, better would be a "folder-dispatcher-and-handler"
+  ;;        of hunchentoot, but it seems to have some problems???
+  ;;       *a second parameter should represent the prefix of the javascript files
+  ;;       *the own javascript code should be extracted of the .html-file into a
+  ;;        javascript file in the javascripts directory
+  (dolist (script '("builder.js" "controls.js" "dragdrop.js" "effects.js" "prototype-1.6.0.3.js" "scriptaculous.js" "slider.js" "sound.js" "unittest.js"))
+    (let ((script-path (concatenate 'string "json/javascripts/" script))
+	  (script-url (concatenate 'string "/javascripts/" script)))
+      (push
+       (create-regex-dispatcher script-url
+				#'(lambda()
+				    (hunchentoot:handle-static-file script-path "text/javascript")))
+       hunchentoot:*dispatch-table*)))
+  ;; === ajax frameworks end ===================================================
   (push
    (create-regex-dispatcher json-get-all-psis #'return-all-topic-psis)
    hunchentoot:*dispatch-table*)
