ac417ee66a38c53f83276a3b3810981975e67dd9
lgiessmann <lgiessmann@Marcs-MBP-2.lan>
2010-08-26 21:37:14 +0000

    fixed a problem with the host prefix in the javascript constants file

--- src/ajax/javascripts/constants.js
+++ src/ajax/javascripts/constants.js
@@ -11,7 +11,7 @@
 
 
 // --- Some constants fot the http connections via the XMLHttpRequest-Object
-var HOST_PREF = /*"http://localhost:8000/";*/ "192.168.178.23:8000/"; // of the form "http://(.+)/"
+var HOST_PREF = "http://localhost:8000/"; /*"192.168.178.23:8000/"; // of the form "http://(.+)/"*/
 var GET_PREFIX = HOST_PREF + "json/get/";
 var GET_STUB_PREFIX = HOST_PREF + "json/topicstubs/";
 var TMCL_TYPE_URL = HOST_PREF + "json/tmcl/type/";
@@ -21,9 +21,9 @@ var ALL_PSIS_URL = HOST_PREF + "json/psis/";
 var TYPE_PSIS_URL = HOST_PREF + "json/tmcl/types/";
 var INSTANCE_PSIS_URL = HOST_PREF + "json/tmcl/instances/";
 var OWN_URL = HOST_PREF + "isidorus";
-var SUMMARY_URL = HOST_PREF + "json/summary"
+var SUMMARY_URL = HOST_PREF + "json/summary";
     var MARK_AS_DELETED_URL = HOST_PREF + "mark-as-deleted";
-var TM_OVERVIEW = "/json/tmcl/overview/";
+var TM_OVERVIEW = HOST_PREF + "json/tmcl/overview/";
 var TIMEOUT = 10000; // const TIMEOUT = 10000 --> "const" doesn't work under IE
 
 
