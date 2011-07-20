9a62b39b28f09e1a354bbdfab05ba825a8fbbf45
lgiessmann <lgiessmann@Marcs-MBP-2.lan>
2011-07-20 13:08:04 +0000

    trunk: the default address of the administrator can be set with the viriable rest-interface:*admin-remote-name* for usage in the RESTful admin handlers

--- src/rest_interface/admin-interface.lisp
+++ src/rest_interface/admin-interface.lisp
@@ -58,7 +58,7 @@
 
 (defun admin-local-backup()
   (handler-case
-      (if (string= "127.0.0.1" (hunchentoot:remote-addr*))
+      (if (string= *admin-remote-name* (hunchentoot:remote-addr*))
 	  (let ((destination-path
 		 (hunchentoot:url-decode (hunchentoot:get-parameter "path"))))
 	    (xtm-exporter:export-as-xtm destination-path :revision 0))
@@ -72,7 +72,7 @@
 
 (defun admin-remote-backup()
   (handler-case
-      (if (string= "127.0.0.1" (hunchentoot:remote-addr*))
+      (if (string= *admin-remote-name* (hunchentoot:remote-addr*))
 	  (progn (hunchentoot:url-decode (hunchentoot:get-parameter "path"))
 		 (setf (hunchentoot:content-type*) "application/xml")
 		 (xtm-exporter:export-as-xtm-string :revision 0))
