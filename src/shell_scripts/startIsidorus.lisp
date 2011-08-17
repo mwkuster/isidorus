90402d38245e584fdfa9b0e7987aa3bdd49ac41d
lgiessmann <lgiessmann@Marcs-MBP-2.lan>
2011-08-17 09:10:46 +0000

    trunk: shell_scrcipts: added basic-authentication to startIsidorus.lisp

--- src/shell_scripts/startIsidorus.lisp
+++ src/shell_scripts/startIsidorus.lisp
@@ -1,6 +1,8 @@
 (setf sb-impl::*default-external-format* :utf-8)
 (asdf:operate 'asdf:load-op 'isidorus)
 ;;(xtm-importer:import-from-xtm "/home/isidorus/.sbcl/site/isidorus/src/unit_tests/textgrid_07.15.2011.xtm" "data_base" :tm-id "http::/isidor.us/tm-1")
+(setf rest-interface:*users* (list (list :uname "isidorus" :passwd "Ar8g7Pw")))
+(setf rest-interface:*use-http-authentication* 1) ;only the host page isidorus.html is protected, note: all RESTful handlers can be invoked without any authentication mechanism!!!    
 (setf rest-interface:*local-backup-remote-address* "143.93.190.247")
 (setf rest-interface:*remote-backup-remote-address* "143.93.190.247")
 (setf rest-interface:*shutdown-remote-address* "127.0.0.1")
