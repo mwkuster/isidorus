956f7e4652b1d07fcdc45ccd02450f2424440558
lgiessmann <lgiessmann@Marcs-MBP-2.lan>
2011-08-17 11:59:20 +0000

    trunk: shell_scripts: adapted the script fillDB.sh for the TextGrid isidorus server and user

--- src/shell_scripts/fillDB.sh
+++ src/shell_scripts/fillDB.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export LANG=de_DE.UTF-8
 cd /home/isidorus/.sbcl/site/isidorus/src
-sbcl << END
+su -l -s /bin/bash isidorus -c sbcl << END
 (setf sb-impl::*default-external-format* :UTF-8)
 (asdf:operate 'asdf:load-op 'isidorus)
-(xtm-importer:setup-repository "/home/isidorus/textgrid.xtm" "/home/isidorus/data_base" :tm-id "http://textgrid.org/serviceregistry/services-tm")
+(xtm-importer:setup-repository "/home/isidorus/latestDump20110711.xtm" "/home/isidorus/data_base" :tm-id "http://textgrid.org/serviceregistry/services-tm")
 (elephant:close-store)
 END
