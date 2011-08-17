b8a72a58ffdc271db787c6ca4aa8c52111ac69df
lgiessmann <lgiessmann@Marcs-MBP-2.lan>
2011-08-17 13:33:41 +0000

    trunk: changed some addresses

--- src/shell_scripts/startIsidorus.lisp
+++ src/shell_scripts/startIsidorus.lisp
@@ -3,10 +3,10 @@
 ;;(xtm-importer:import-from-xtm "/home/isidorus/.sbcl/site/isidorus/src/unit_tests/textgrid_07.15.2011.xtm" "data_base" :tm-id "http::/isidor.us/tm-1")
 (setf rest-interface:*users* (list (list :uname "isidorus" :passwd "Ar8g7Pw")))
 (setf rest-interface:*use-http-authentication* 2) ;only the host page isidorus.html and the commit handlers are protected  
-(setf rest-interface:*local-backup-remote-address* "143.93.190.247") ;the allowed other remove peer ip
-(setf rest-interface:*remote-backup-remote-address* "143.93.190.247") ;the allowed other remove peer ip
+(setf rest-interface:*local-backup-remote-address* "12.34.56.78") ;the allowed other remove peer ip
+(setf rest-interface:*remote-backup-remote-address* "12.34.56.78") ;the allowed other remove peer ip
 (setf rest-interface:*shutdown-remote-address* "127.0.0.1") ;the allowed other remove peer ip
-(rest-interface:start-json-engine "/home/isidorus/data_base" :host-name "143.93.190.176" :port 7000)
+(rest-interface:start-json-engine "/home/isidorus/data_base" :host-name "12.34.56,78" :port 12345)
 (rest-interface:start-admin-server)
 (format t "will enter die-when-finished")
 (rest-interface:die-when-finished)
