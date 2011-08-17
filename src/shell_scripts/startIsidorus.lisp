922f8ed6ff2d14a4b355f13425e4a4fbd3835ffb
lgiessmann <lgiessmann@Marcs-MBP-2.lan>
2011-08-17 09:04:12 +0000

    trunk: shell_scripts: adapted the startIsidorus.lisp script to the latest changes done in TextGrid

--- src/shell_scripts/startIsidorus.lisp
+++ src/shell_scripts/startIsidorus.lisp
@@ -1,10 +1,11 @@
 (setf sb-impl::*default-external-format* :utf-8)
 (asdf:operate 'asdf:load-op 'isidorus)
-;;(xtm-importer:import-from-xtm "/home/servreg/.sbcl/site/isidorus/src/unit_tests/textgrid_07.15.2011.xtm" "data_base" :tm-id "http::/isidor.us/tm-1")
+;;(xtm-importer:import-from-xtm "/home/isidorus/.sbcl/site/isidorus/src/unit_tests/textgrid_07.15.2011.xtm" "data_base" :tm-id "http::/isidor.us/tm-1")
 (setf rest-interface:*local-backup-remote-address* "143.93.190.247")
 (setf rest-interface:*remote-backup-remote-address* "143.93.190.247")
 (setf rest-interface:*shutdown-remote-address* "127.0.0.1")
-(rest-interface:start-json-engine "/home/servreg/data_base" :host-name "143.93.190.176" :port 7000)
+(rest-interface:start-json-engine "/home/isidorus/data_base" :host-name "143.93.190.176" :port 7000)
 (rest-interface:start-admin-server)
+(format t "will enter die-when-finished")
 (rest-interface:die-when-finished)
 
