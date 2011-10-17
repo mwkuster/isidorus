c20942434fcb39f7c1083d4d096caf02a294454e
lgiessmann <lgiessmann@Marcs-MBP-2.lan>
2011-10-17 15:14:12 +0000

    gdl-frontend: Widgets: fixed some bugs in the TMCL-definition for the new (test-)GDL-Schema; implemented a new test-class

--- src/anaToMia/GDL_Widgets/src/us/isidor/gdl/anaToMia/Widgets/base/TestClass.java
+++ src/anaToMia/GDL_Widgets/src/us/isidor/gdl/anaToMia/Widgets/base/TestClass.java
@@ -1,33 +1,89 @@
 package us.isidor.gdl.anaToMia.Widgets.base;
 
-
-import us.isidor.gdl.anaToMia.Widgets.isidorus.LoadScreenPanel;
-
+import us.isidor.gdl.anaToMia.TmEngine.jtmsBasedEngine.JtmsTmEngine;
+import us.isidor.gdl.anaToMia.TopicMaps.TmEngineModel.TmEngine;
+import us.isidor.gdl.anaToMia.TopicMaps.TopicMapsModel.Construct;
+import us.isidor.gdl.anaToMia.TopicMaps.TopicMapsModel.TopicMapsTypes;
+import us.isidor.gdl.anaToMia.Widgets.isidorus.CommitCallback;
+import us.isidor.gdl.anaToMia.Widgets.isidorus.DeleteCallback;
+import us.isidor.gdl.anaToMia.Widgets.isidorus.LoadSchemaCallback;
+import us.isidor.gdl.anaToMia.Widgets.environment.ICommitCallback;
+import us.isidor.gdl.anaToMia.Widgets.environment.IDeleteCallback;
+import us.isidor.gdl.anaToMia.Widgets.environment.ILoadSchemaCallback;
+import us.isidor.gdl.anaToMia.Widgets.environment.Pair;
+import us.isidor.gdl.anaToMia.Widgets.environment.TopicIdentifierTypes;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.Timer;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.dom.client.TextAreaElement;
+import java.util.ArrayList;
 
 
 public class TestClass implements EntryPoint{
+	public final String MODEL_PREF = "http://services.org/serviceregistry/model/types/";
+	public final String SERVICE_PSI = MODEL_PREF + "Service";
+	public final String SORT_SERVICE = "http://service.org/serviceregistry/psis/service/sort-service";
+	HorizontalPanel mainPanel = new HorizontalPanel();
+	GdlPanel gdlPanel = null;
+
 	@Override
 	public void onModuleLoad() {
-		RootPanel.get().setHeight("1500px");
-		RootPanel.get().setWidth("2500px");
-		Window.enableScrolling(false);
-		final LoadScreenPanel lsp = new LoadScreenPanel("Title", "message");
-		RootPanel.get().add(lsp);
-		
-		Timer t = new Timer() {
-			@Override
-			public void run() {
-				lsp.removeFromParent();
-				Window.enableScrolling(true);
-			}
-		};
-		
-		t.schedule(5000);
-		
-		
+		try{
+			RootPanel.get("GWT_Content").add(mainPanel);
+			mainPanel.setBorderWidth(1);
+			mainPanel.setPixelSize(1000, 600);
+
+			ArrayList<Pair<String, TopicIdentifierTypes>> requestedTopicsToCreate = new ArrayList<Pair<String,TopicIdentifierTypes>>();
+			requestedTopicsToCreate.add(new Pair<String, TopicIdentifierTypes>(SERVICE_PSI, TopicIdentifierTypes.SubjectIdentifier));
+			
+			Pair<String, TopicIdentifierTypes> requestedTopicToEdit = new Pair<String, TopicIdentifierTypes>(SORT_SERVICE, TopicIdentifierTypes.SubjectIdentifier);
+			gdlPanel = new GdlPanel(requestedTopicToEdit, null);
+
+			GdlPanel.addClickHandler("hash_object_reset_button_id", new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent event) {
+					Element elem = DOM.getElementById("hash_object_text_key_id__GDL_0");
+					((TextAreaElement)elem).setValue("");
+					elem = DOM.getElementById("hash_object_text_value_id__GDL_0");
+					((TextAreaElement)elem).setValue("");
+				}
+			});
+
+			mainPanel.add(gdlPanel);
+			gdlPanel.setTmEngine(new JtmsTmEngine());
+			gdlPanel.setLoadSchemaCallback(new TestLoadSchemaCallback());
+			gdlPanel.setCommitCallback(new ICommitCallback() {
+				@Override
+				public void commitTmConstruct(ArrayList<Pair<Construct, TopicMapsTypes>> constructs, String buttonId, TmEngine tmEngine) {
+					Window.alert("not implemented yet :-(");
+				}
+			});
+			gdlPanel.setDeleteCallback(new IDeleteCallback() {
+				@Override
+				public void deleteTmConstruct(ArrayList<Pair<Construct, TopicMapsTypes>> constructs, TmEngine tmEngine, String buttonId) {
+					Window.alert("not implemented yet :-(");
+				}
+			});
+
+			Button requestButton = new Button("load schema");
+			requestButton.addClickHandler(new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent event) {
+					gdlPanel.loadSchema();
+				}
+			});
+
+			mainPanel.add(requestButton);
+		}catch(Exception e){
+			e.printStackTrace();
+			Window.alert(">> e >> " + e.getClass() + " >> " + e.getMessage());
+		}
 	}
 }
---
+++ src/anaToMia/GDL_Widgets/src/us/isidor/gdl/anaToMia/Widgets/base/TestLoadSchemaCallback.java
@@ -0,0 +1,122 @@
+package us.isidor.gdl.anaToMia.Widgets.base;
+
+
+import java.util.ArrayList;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.user.client.Window;
+import us.isidor.gdl.anaToMia.Widgets.base.ButtonDialog;
+import us.isidor.gdl.anaToMia.Widgets.base.GdlPanel;
+import us.isidor.gdl.anaToMia.Widgets.environment.ExecutionException;
+import us.isidor.gdl.anaToMia.Widgets.environment.ILoadSchemaCallback;
+import us.isidor.gdl.anaToMia.Widgets.environment.Pair;
+import us.isidor.gdl.anaToMia.Widgets.environment.TopicIdentifierTypes;
+
+
+// this class can be used as a callback that requests the Topic Map data
+// addressable by the URI requestUrl
+public class TestLoadSchemaCallback implements ILoadSchemaCallback{
+	private final String requestUrl = URL.encode(GWT.getModuleBaseURL() + "Service_GDL_Schema.jtm");
+	private ArrayList<Pair<String, TopicIdentifierTypes>> requestedTopicsToCreate = new ArrayList<Pair<String,TopicIdentifierTypes>>(); 
+	private Pair<String, TopicIdentifierTypes> requestedTopicToEdit = null;
+	private RequestBuilder requestBuilder = new RequestBuilder(RequestBuilder.GET, requestUrl);
+
+
+	public TestLoadSchemaCallback(){}
+
+
+	// this method is invoked as a callback method
+	@Override
+	public void loadSchema(GdlPanel panel, Pair<String, TopicIdentifierTypes> requestedTopicToEdit , ArrayList<Pair<String, TopicIdentifierTypes>> requestedTopicsToCreate)throws RequestException {
+		this.requestedTopicsToCreate = requestedTopicsToCreate;
+		this.requestedTopicToEdit = requestedTopicToEdit;
+		requestBuilder.sendRequest(null, new RequestCallbackImpl(panel));
+	}
+
+
+	// this method implements the actual request and a simple error handling
+	private class RequestCallbackImpl implements RequestCallback{
+		private GdlPanel panel = null;
+
+
+		@SuppressWarnings("unused")
+		private RequestCallbackImpl() {}
+
+
+		public RequestCallbackImpl(GdlPanel panel){
+			this.panel = panel;
+		}
+
+
+		@Override
+		public void onResponseReceived(Request request, Response response) {
+			if(Response.SC_OK == response.getStatusCode()){
+				try{
+					if(panel.getTmEngine() == null || panel.getSchemaTm() == null) throw new ExecutionException("no Topic Maps engine was set yet");
+					panel.getTmEngine().importTopicMap(response.getText(), panel.getSchemaTm());
+					panel.createView();
+				}catch(ExecutionException e){
+					Window.alert("Execution Error: " + e.getMessage());
+				}catch(Exception e){
+					Window.alert("panel: " + panel + ", tm: " + panel.getSchemaTm() + "\ncaught error: " + e.getLocalizedMessage()); // TODO: implement smarter error handling
+					e.printStackTrace();
+				}
+			} else {
+				final ButtonDialog dialog = new ButtonDialog("Connection Error", "The request to " + requestUrl + " failed\n" + response.getStatusCode() + ": " + response.getStatusText(), "retry", "cancel", null, null);
+				dialog.setLeftButtonClickHandler(new ClickHandler() {   
+					@Override
+					public void onClick(ClickEvent event) {
+						dialog.hide();
+						try{
+							loadSchema(panel, TestLoadSchemaCallback.this.requestedTopicToEdit, TestLoadSchemaCallback.this.requestedTopicsToCreate);
+						}catch(Exception e){
+							Window.alert("connection to : " + requestUrl + " failed: " + e.getMessage());
+						}
+					}
+				});
+
+				dialog.setRightButtonClickHandler(new ClickHandler() {
+					@Override
+					public void onClick(ClickEvent event) {
+						dialog.hide();
+					}
+				});
+
+				dialog.center();
+			}
+		}
+
+
+		@Override
+		public void onError(Request request, Throwable exception) {
+			final ButtonDialog dialog = new ButtonDialog("Connection Error", "The request to " + requestUrl + " failed\n" + exception.getMessage(), "retry", "cancel", null, null);
+			dialog.setLeftButtonClickHandler(new ClickHandler() {   
+				@Override
+				public void onClick(ClickEvent event) {
+					dialog.hide();
+					try{
+						loadSchema(panel, TestLoadSchemaCallback.this.requestedTopicToEdit, TestLoadSchemaCallback.this.requestedTopicsToCreate);
+					}catch(Exception e){
+						Window.alert("connection to : " + requestUrl + " failed: " + e.getMessage());
+					}
+				}
+			});
+
+			dialog.setRightButtonClickHandler(new ClickHandler() {
+				@Override
+				public void onClick(ClickEvent event) {
+					dialog.hide();
+				}
+			});
+
+			dialog.center();
+		}
+	}
+}
